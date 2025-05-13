38c2706e75ffa9b2401cbaa0382630abea5f9224
Teyi Adzufeh <63776894+teyiadzufeh@users.noreply.github.com>
2025-05-13 13:10:38 +0100

    fix: category issue

--- src/db/product.schema.ts
+++ src/db/product.schema.ts
@@ -4,7 +4,6 @@ export const ProductSchema = z.object({
     name: z.string().min(1),
     description: z.string().min(1),
     price: z.number().min(0),
-    category: z.string().min(1),
     image_url: z.string().url().optional(),
 });
 
@@ -16,7 +15,6 @@ export type ProductType = {
     name?: string;
     description?: string;
     price?: number;
-    category: string;
     image_url?: string;
     created_at?: Date;
     updated_at?: Date;
